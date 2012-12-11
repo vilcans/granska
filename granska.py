60ea6114196c41d55a8ea86a103b62ea75339068
Martin Vilcans <martin@librador.com>
2012-12-11 14:43:04 +0100

    Show summary and hide empty types

--- granska.py
+++ granska.py
@@ -10,27 +10,29 @@ types = (
     'REVIEW',
     'TODO',
 )
-type_to_items = dict((t, []) for t in types)
+type_to_items = {}
 
 comment_re = re.compile('(' + '|'.join(types) + r')\s*:\s*(.*)')
 
-files = sys.argv[1:]
-files.sort()
-
 items = []
 
-for file in files:
-    stream = open(file, 'r')
-    for line_number, line in enumerate(stream):
-        match = comment_re.search(line)
-        if match:
-            item = {
-                'file': file,
-                'line': line_number + 1,
-                'type': match.group(1),
-                'comment': match.group(2),
-            }
-            type_to_items[item['type']].append(item)
-    stream.close()
+def scan_files(files):
+    for file in files:
+        stream = open(file, 'r')
+        for line_number, line in enumerate(stream):
+            match = comment_re.search(line)
+            if match:
+                item = {
+                    'file': file,
+                    'line': line_number + 1,
+                    'type': match.group(1),
+                    'comment': match.group(2),
+                }
+                type_to_items.setdefault(item['type'], []).append(item)
+        stream.close()
+
+files = sys.argv[1:]
+files.sort()
+scan_files(files)
 
 print template.render(type_to_items=type_to_items, types=types)
